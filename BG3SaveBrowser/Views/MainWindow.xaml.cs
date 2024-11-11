3dc8488e2d928247686d357ec236b2fa023c963e
Kyle Lambert <kylelambert101@gmail.com>
2024-11-10 21:18:35 -0500

    Read lots of directories and show results

--- BG3SaveBrowser/Views/MainWindow.xaml.cs
+++ BG3SaveBrowser/Views/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.ObjectModel;
 using System.IO;
 using System.Windows;
+using BG3SaveBrowser.Infrastructure.Services;
+using BG3SaveBrowser.Models;
 using Path = System.IO.Path;
 
-namespace BG3SaveBrowser;
+namespace BG3SaveBrowser.Views;
 public partial class MainWindow : Window
 {
     public MainWindow()
@@ -12,7 +14,7 @@ public partial class MainWindow : Window
         FilesListView.ItemsSource = Files;
     }
 
-    public ObservableCollection<FileItem> Files { get; set; } = new();
+    private ObservableCollection<FileItem> Files { get; set; } = new();
 
     private void BrowseButton_Click(object sender, RoutedEventArgs e)
     {
@@ -26,19 +28,11 @@ public partial class MainWindow : Window
 
     private void LoadFiles(string directoryPath)
     {
+        var processor = new FileProcessor();
         Files.Clear();
-        var files = Directory.GetFiles(directoryPath);
-        foreach (var filePath in files)
-            Files.Add(new FileItem
-            {
-                Name = Path.GetFileName(filePath),
-                Path = filePath
-            });
+        foreach (var x in processor.ProcessPath(directoryPath))
+        {
+            Files.Add(x);
+        }
     }
-}
-
-public class FileItem
-{
-    public string Name { get; set; }
-    public string Path { get; set; }
 }
