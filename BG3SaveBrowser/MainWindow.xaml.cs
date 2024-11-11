84c2abf27413800622fff49a67067382020b2423
Kyle Lambert <kylelambert101@gmail.com>
2024-11-10 20:32:38 -0500

    Create basic file browser

--- BG3SaveBrowser/MainWindow.xaml.cs
+++ BG3SaveBrowser/MainWindow.xaml.cs
@@ -1,23 +1,44 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace BG3SaveBrowser;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        FilesListView.ItemsSource = Files;
     }
+
+    public ObservableCollection<FileItem> Files { get; set; } = new();
+
+    private void BrowseButton_Click(object sender, RoutedEventArgs e)
+    {
+        using var folderDialog = new FolderBrowserDialog();
+        if (folderDialog.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+        
+        var selectedPath = folderDialog.SelectedPath;
+        DirectoryPathTextBox.Text = selectedPath;
+        LoadFiles(selectedPath);
+    }
+
+    private void LoadFiles(string directoryPath)
+    {
+        Files.Clear();
+        var files = Directory.GetFiles(directoryPath);
+        foreach (var filePath in files)
+            Files.Add(new FileItem
+            {
+                Name = Path.GetFileName(filePath),
+                Path = filePath
+            });
+    }
+}
+
+public class FileItem
+{
+    public string Name { get; set; }
+    public string Path { get; set; }
 }
