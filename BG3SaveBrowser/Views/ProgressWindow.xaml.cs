2c034c5583006a512ff798393e5ba14c990054b8
Kyle Lambert <kylelambert101@gmail.com>
2024-12-01 09:58:27 -0500

    add empty progress window

---
+++ BG3SaveBrowser/Views/ProgressWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace BG3SaveBrowser.Views;
+
+public partial class ProgressWindow : Window
+{
+    public ProgressWindow()
+    {
+        InitializeComponent();
+    }
+}
