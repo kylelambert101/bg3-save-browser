dc3ec8b6c4488ed68b5b418f93d2ebcd69adae82
Kyle Lambert <kylelambert101@gmail.com>
2024-12-04 12:24:03 -0500

    Convert Save Time to local time

---
+++ BG3SaveBrowser/Infrastructure/Utilities/UtcToLocalDateTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace BG3SaveBrowser.Infrastructure.Utilities;
+
+public class UtcToLocalDateTimeConverter : IValueConverter
+{
+public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+{
+    try
+    {
+        return DateTime.SpecifyKind(DateTime.Parse(value.ToString()), DateTimeKind.Utc).ToLocalTime();
+    }
+    catch
+    {
+        return null;
+    }
+}
+
+public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+    throw new NotImplementedException();
+}
+}
